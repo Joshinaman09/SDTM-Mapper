e707df0188e5000844379acfe00fd80c09177ca1
Joshinaman09 <100407973+Joshinaman09@users.noreply.github.com>
2026-03-11 12:28:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -120,7 +120,7 @@ sequence_field = cfg.get("sequence_field")
 # FILE UPLOAD
 # ===============================
 uploaded_files = [
-    "LAB.xpt", "LAB1.xpt", "LAB2.xpt", "labcode.xpt", "VS.xpt"
+    "/mnt/data/LAB.xpt", "/mnt/data/LAB1.xpt", "/mnt/data/LAB2.xpt", "/mnt/data/labcode.xpt", "/mnt/data/VS.xpt"
 ]
 
 # ===============================
@@ -372,4 +372,3 @@ st.download_button(
     file_name=f"{domain}.csv",
     mime="text/csv"
 )
-
